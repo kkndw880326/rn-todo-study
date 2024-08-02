566c92d1f35ac63b970eee9d6f937a3b2a84259d
soojin <kkndw880326@gmail.com>
2024-08-02 22:25:02 +0900

    feat: add image type

--- src/screens/SignInScreen.tsx
+++ src/screens/SignInScreen.tsx
@@ -1,8 +1,10 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import TestImage from '../../assets/test.png';
 
 const SignInScreen = () => {
   return (
     <View style={styles.container}>
+      <Image source={TestImage} style={styles.image}></Image>
       <Text>SignInScreen</Text>
     </View>
   );
@@ -13,6 +15,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  image: {
+    width: 200,
+    height: 200,
   },
 });
 
